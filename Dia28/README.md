364884a94f9d91a9d8a5043f62e403617788d181
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-26 04:05:25 -0300

    day 28

---
+++ Dia28/README.md
@@ -0,0 +1,17 @@
+## [Dia27]🔘 Mentalista com tentativas (imersao Alura)!
+
+<div align="center">
+  <img height="450em" src= ./alura.png>
+</div>
+
+##
+
+O que aprendi:
+
+- parsetInt
+- getElement
+- innerHTML
+- Math.random
+
+## Link do site:
+https://pollysantos.github.io/Desafio_30dias_Resilia/Dia28/
---
+++ Dia28/script.js
@@ -0,0 +1,46 @@
+var numeroSecreto = parseInt(Math.random () * 11);
+var qtdChute = 10;
+var dificuldade = 1;
+
+function Chutar(){
+  
+  var elementoResultado = document.getElementById("resultado")
+  var chutesRestantes = document.getElementById("chutes")
+  var chute = parseInt(document.getElementById("valor").value)
+  console.log(chute)
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "VOCÊ ACERTOU"
+    console.log("acertou")
+    numeroSecreto = parseInt(Math.random () * 11);
+    //Chutar(8)
+  } else if (chute > 10 || chute < 0) 
+  { 
+    elementoResultado.innerHTML = "Você deve digitar um número entre 0 e 10";
+  }
+  else {
+    qtdChute = qtdChute - 1;
+   if (qtdChute <= 0){
+     
+     elementoResultado.innerHTML = "Você Perdeu, Recarregue a pagina para tentar      novamente!";
+     chutesRestantes.innerHTML = "Você tem " + qtdChute + " Chutes Restantes!"
+     //Chutar(12);
+     
+   }
+   
+   else {
+     if (chute > numeroSecreto) {
+       elementoResultado.innerHTML = "Você          errou,tente novamente! Seu Palpite \(" + chute + ") é maior que o Numero Secreto" ;
+     }
+     else{
+       elementoResultado.innerHTML = "Você          errou,tente novamente! Seu Palpite \(" + chute + ") é menor que o Numero Secreto" ;
+     }
+     
+     chutesRestantes.innerHTML = "Você tem " + qtdChute + " Chutes Restantes!"
+   }
+   
+   
+         }
+}
+
+//Colocar informacao de 0 a 10 mais que isso erro 
+// == 
